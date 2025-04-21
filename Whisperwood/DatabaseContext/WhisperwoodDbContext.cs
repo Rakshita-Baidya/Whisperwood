f1a8922f7195fed9097f014ba360229df4949523
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2025-04-21 15:31:53 +0545

    Added Database configration and other packages

---
+++ Whisperwood/DatabaseContext/WhisperwoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Whisperwood.Models;
+
+namespace Whisperwood.DatabaseContext
+{
+    public class WhisperwoodDbContext : DbContext
+    {
+        public WhisperwoodDbContext(DbContextOptions<WhisperwoodDbContext> options)
+            : base(options) { }
+
+        public DbSet<Books> Books { get; set; }
+        public DbSet<CoverImages> CoverImages { get; set; }
+        public DbSet<Authors> Authors { get; set; }
+
+    }
+}
