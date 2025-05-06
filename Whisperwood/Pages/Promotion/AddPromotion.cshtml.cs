e67e5d4260d3015c0bdeee651e769bdc3e7116ee
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2025-05-06 23:41:41 +0545

    add promotion view and logic added

---
+++ Whisperwood/Pages/Promotion/AddPromotion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Whisperwood.Pages.Promotion
+{
+    public class AddPromotionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
