17659d47b3b64685d51cdb2451d39a545e62b82a
Bidur <nbidur231@gmail.com>
2025-05-07 06:51:55 +0545

    nth much

---
+++ Whisperwood/pages/Book/AddBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Whisperwood.pages.Book
+{
+    public class AddBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
