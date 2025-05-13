b244516328881b984ce081c616972d8a1e108a5d
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2025-05-13 12:04:14 +0545

    added admin side author crud

---
+++ Whisperwood/Pages/Author/AddAuthor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Whisperwood.Pages.Author
+{
+    public class AddAuthorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Whisperwood/Pages/Author/ManageAuthor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Whisperwood.Pages.Author
+{
+    public class ManageAuthorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
